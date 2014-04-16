d00eb071ba07d23ffb306cf30ec7b5ae30412af2
Hristo Danovski <hristo.danovski@omg-m.com>
2014-04-16 11:43:18 +0300

    Merged contents

---
+++ src/main/java/com/omg/javafxarticles/main/Screens.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.omg.javafxarticles.main;
+
+/**
+ *
+ * @author default
+ */
+public interface Screens {
+    
+    public static final String LOGIN_SCREEN = "LoginScreen";
+    public static final String LOGIN_SCREEN_FXML = "/fxml/login/LoginScene.fxml";
+    
+    public static final String ARTICLES_SCREEN = "ArticlesScreen";
+    public static final String ARTICLES_SCREEN_XML = "/fxml/articles/ArticlesScene.fxml";
+    
+    
+}
